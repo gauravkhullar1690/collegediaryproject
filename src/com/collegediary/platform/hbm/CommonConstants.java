ec43e0d1f7e9db17c923834c10c4f7b057afd870
unknown <goravdhiman46@gmail.com>
2013-12-13 11:11:58 +0530

    String Utils & Common Constants file added

---
+++ src/com/collegediary/platform/hbm/CommonConstants.java
@@ -0,0 +1,9 @@
+package com.collegediary.platform.hbm;
+
+public class CommonConstants 
+{
+	public static String SPRING_SECURITY_REMEMBER_ME_COOKIE_KEY_REST_SERVICES = "collegeDiaryRemmberMeKey";
+	public static String REST_SERVICES_COOKIE_KEY = "collegeDiary_REST_SERVICE";
+	public static int EXPIRYTIME = 1800;
+	public static String DOMAIN = "http://localHost:8080/";
+}
